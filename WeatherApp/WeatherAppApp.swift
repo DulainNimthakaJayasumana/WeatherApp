f560d35969dd4e970c12af056bb4529de6625dbc
DulainNimthakaJayasumana <59439269+DulainNimthakaJayasumana@users.noreply.github.com>
2025-02-04 16:03:30 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dulain Jayasumana on 2025-02-07.
+//
+
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
